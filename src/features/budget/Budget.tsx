18875515925cdd90f0af7623fd41a696d4ea9646
dobrin.rusev <dobrin.rusev@pivotenergy.com>
2025-05-10 12:50:47 +0300

    latest

--- src/features/budget/Budget.tsx
+++ src/features/budget/Budget.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import DatePicker from "react-datepicker";
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { decrement, processFile, selectTransactions, selectSpend, selectReceived } from './budgetSlice';
 import styles from './Budget.module.css';
@@ -8,12 +6,11 @@ import { useFilePicker } from 'use-file-picker';
 import "react-datepicker/dist/react-datepicker.css";
 import { TransactionTable } from './TransactionTable';
 import { Categories, getCategoryAmount, getCategoryTransactions } from './BudgetParser';
+import { OverallSpending } from './OverallSpending';
 
 export function Budget() {
 
   const dispatch = useAppDispatch();
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
   const transactions = useAppSelector(selectTransactions)
   const spend = useAppSelector(selectSpend)
   const received = useAppSelector(selectReceived)
@@ -37,23 +34,20 @@ export function Budget() {
       </div>
       <div className={styles.divider}></div>
       <div className={styles.dataPicket}>
-        {/* <label >От дата</label>
-        <div className="field-quarter">
-          <DatePicker selected={startDate} onChange={(date: any) => setStartDate(date)} />
-        </div>
-        <label>До дата</label>
-        <div className="field-quarter-right">
-          <DatePicker selected={endDate} onChange={(date: any) => setEndDate(date)} />
-        </div> */}
         <button onClick={() => dispatch(decrement())}> Покажи </button>
         <button onClick={() => openFileSelector()}> Избери Файл </button>
       </div>
+      {transactions && <OverallSpending
+        transactions={transactions}
+        spend={spend}
+        received={received}
+      />
+      }
       <div className={styles.divider}></div>
       <div className={styles.divider}></div>
       {transactions && Categories.map((category, index) => {
         const categoryTransactions = getCategoryTransactions(category[0], transactions)
         const categorySpend = getCategoryAmount(categoryTransactions, 'debit')
-        console.log(categoryTransactions, categorySpend)
         return <TransactionTable
           key={index}
           title={category[0]}
--- src/features/budget/BudgetParser.ts
+++ src/features/budget/BudgetParser.ts
@@ -4,16 +4,16 @@ const LINE_START_DEFAULT = 1
 
 export const Categories = [
     ['Хранителни Покупки', 'Alex Trade', 'BILLA', 'METRO', 'LIDL', 'KAUFLAND', 'ALEX TRADE', 'STOP4ETO', '#DM', 'Metro Cash & Carry',
-        'BAHARICA', 'T-MARKET', 'LILLY', 'EBAG.BG', 'PRI RENA BAKERY', 'DZHOTO'],
+        'BAHARICA', 'T-MARKET', 'LILLY', 'EBAG.BG', 'PRI RENA BAKERY', 'DZHOTO', 'PETROZARA'],
     ['Здраве', 'SOPHARMACY', 'FARMA VISION', 'PHARMACY DARA 5 PLO', 'FRAMAR', 'MARESHKI'],
-    ['Гориво', 'GAZPROM', 'OMV', 'SCHELL', 'PETROL', 'SHELL', 'INSAOIL', 'VAS OIL', 'LUKOIL', 'GAS STATIONS EKO'],
-    ['За колата', 'MOTION SERVICE', 'API BG TOLL', 'СКОРПИОН ИНС ООД', 'ITS VINETKI SOF'],
+    ['Транспорт', 'GAZPROM', 'OMV', 'SCHELL', 'PETROL', 'SHELL', 'INSAOIL', 'VAS OIL', 'LUKOIL', 'GAS STATIONS EKO', 'MOTION SERVICE', 'API BG TOLL', 'СКОРПИОН ИНС ООД', 'ITS VINETKI SOF'],
     ['Ресторанти', 'JAGERHOF', 'HAPPY BAR & GRILL', 'JAPONICA', 'Q BAR', 'HAPPY BAR&GRILL', 'JEGERHOF', 'PLPLAZ', 'DOMINOS', 'UGOVSKIA',
         'WEST COFFE PLAZA', 'GaleriaStara', 'GRAFIT', 'VIKTORIA', 'TORRO GRANDE', 'CONFETTI', 'EDO SUSH', 'BAR DINUR', 'TAMSHOUSE', 
         'LA KUCHINA', 'Glovo'],
     ['Банки', 'Теглене ПОС', 'FIB_ATM', 'UNICREDIT', 'ALLIANZ', 'Теглене ПОС BUL VASIL APRILOV'],
     ['За работа', 'AY STAYL', 'КОМФОРТ', 'АВАНСОВО ДОД', 'ДЗПО', 'ДОО', 'ЗОВ', 'REGIONAL BUSINESS', 'Regus Kamenitza Park'],
     ['Ежемесечни', 'МАТЕЙ', 'Бистра', 'EPAY A1 MOBILE', 'ИЗИПЕЙ', 'ATHLETIC FITNESS'],
+    ['Revolut','Revolut'],
     ['Пазаруване', 'HITA', 'PEPCO', 'STUDIO MODERNA SOF', 'SPEEDY', 'TECHNOPOLIS', 'answear.bg', 'HUMANA', 'SPORT VISION', 'JUMBO', 
         'TOM TAILOR', 'JAGERSHOP', 'PEPKO', 'ECONT', 'ART 93', 'H&M', 'ART 93', 'KRISTI STIL', 'SPIDI'],
     ['Хоби', 'WEEKEND WOODWORKER', 'OZONE.BG', 'DECATLON', 'EVENTIM'],
@@ -21,7 +21,8 @@ export const Categories = [
         'Погасяване редовна лихва кредит', 'PRAKTIKER', 'BRICOLAGE', 'BAUMAX', 'HOMEMAX', 'TULS OOD', 'HIPERMARKET RILA', 'PARKET STYLE',
         'TOPLIVO', 'IZIDOR', 'СЪЛИВЕР'],
     ['Чужбина', 'Плащане чрез ПОС чужбина'],
-    ['UNDEFINED']
+    ['Такси', 'Такса за операция от тип', 'Такса Нар.превод,IB', 'Такса за нареден', 'Такса поддръжка карта'],
+    ['Без Категория']
 ]
 
 export const parse = (fileContent: string) => {
@@ -52,7 +53,7 @@ export const parse = (fileContent: string) => {
 const match = (str: string, pattern: string) => str.indexOf(pattern) !== -1
 
 const getTransactionCategory = (contragent: string, document: string, reason: string, info: string) => {
-    let result = 'UNDEFINED'
+    let result = 'Без Категория'
     Categories.forEach((category) => {
         category.forEach((pattern, pIndex) => {
             if (pIndex === 0)
